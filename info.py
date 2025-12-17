45b2942e187a6b2907d5d27af029f40ddf8580c5
fax008f-coder <fax008f@gmail.com>
2025-12-17 20:31:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -148,7 +148,7 @@ PAYMENT_TEXT = environ.get(
 # 🔹 CLONE SETTINGS
 # ============================================================
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'AnimeZerox') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
@@ -276,6 +276,7 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 
